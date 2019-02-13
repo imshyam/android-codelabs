84d4b18f85d996f75e32f30eff802e8a5353b638
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-13 22:22:06 +0800

    add async task module

---
+++ 0213asynctask/src/main/java/com/shyam/a0213asynctask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shyam.a0213asynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
