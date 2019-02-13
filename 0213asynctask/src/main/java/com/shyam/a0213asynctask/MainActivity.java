5e43a47bc496238475007a162ed747f4bae8d19f
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-14 00:56:39 +0800

    handle click

--- 0213asynctask/src/main/java/com/shyam/a0213asynctask/MainActivity.java
+++ 0213asynctask/src/main/java/com/shyam/a0213asynctask/MainActivity.java
@@ -2,12 +2,22 @@ package com.shyam.a0213asynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void startAsync(View view) {
+        new MyAsyncTask(textView).execute();
     }
 }
