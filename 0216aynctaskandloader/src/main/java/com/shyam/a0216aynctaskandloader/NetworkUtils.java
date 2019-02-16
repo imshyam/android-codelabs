0d354f36f5c95f18d410b5c4b157972c6a2cd584
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-16 13:34:35 +0800

    add network util to make network calls

---
+++ 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/NetworkUtils.java
@@ -0,0 +1,71 @@
+package com.shyam.a0216aynctaskandloader;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+    final static String LOG_TAG = NetworkUtils.class.getSimpleName();
+    // Base URL for Books API.
+    private static final String BOOK_BASE_URL =  "https://www.googleapis.com/books/v1/volumes?";
+    // Parameter for the search string.
+    private static final String QUERY_PARAM = "q";
+    // Parameter that limits search results.
+    private static final String MAX_RESULTS = "maxResults";
+    // Parameter to filter by print type.
+    private static final String PRINT_TYPE = "printType";
+
+    static String getBooks(String query) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String response = null;
+
+        try {
+            Uri uri = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, query)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+            URL url = new URL(uri.toString());
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append("\n"); // Not necessary but help debugging
+            }
+
+            if(stringBuilder.length() > 0) {
+                response = stringBuilder.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Log.d(LOG_TAG, response);
+        return response;
+    }
+}
