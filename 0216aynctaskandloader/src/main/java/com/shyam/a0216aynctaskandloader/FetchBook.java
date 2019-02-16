988cea61e3d509c4fc6a7d2c32d56871913b2c7c
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-16 13:47:24 +0800

    parse and print response

--- 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/FetchBook.java
+++ 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/FetchBook.java
@@ -3,6 +3,10 @@ package com.shyam.a0216aynctaskandloader;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
@@ -22,5 +26,35 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = jsonObject.getJSONArray("items");
+            int i = 0;
+            String title = null;
+            String author = null;
+            while (i < jsonArray.length() && (author == null && title == null)) {
+                JSONObject book = jsonArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                try {
+                    title = volumeInfo.getString("title");
+                    author = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                i++;
+            }
+            if (author != null && title != null) {
+                titleText.get().setText(title);
+                authorText.get().setText(author);
+            } else {
+                titleText.get().setText(R.string.no_result);
+                authorText.get().setText("");
+            }
+        } catch (Exception e) {
+            titleText.get().setText(R.string.no_result);
+            authorText.get().setText("");
+        }
     }
 }
