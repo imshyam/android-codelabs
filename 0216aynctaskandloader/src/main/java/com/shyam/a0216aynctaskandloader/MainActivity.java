ecd045a20b9b12be50e376e8e881b9ea1baddd8c
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-16 13:35:06 +0800

    run async task

--- 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/MainActivity.java
+++ 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/MainActivity.java
@@ -2,12 +2,27 @@ package com.shyam.a0216aynctaskandloader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView title, author;
+    private EditText editTextQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.book_name);
+        author = findViewById(R.id.author);
+        editTextQuery = findViewById(R.id.edit_text_title);
+    }
+
+    public void searchBooks(View view) {
+        String queryString = editTextQuery.getText().toString();
+        new FetchBook(title, author).execute(queryString);
     }
 }
