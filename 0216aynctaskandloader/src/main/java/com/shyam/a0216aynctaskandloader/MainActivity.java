d7d0a829fd742609a9b825e5d733fad5a7986f9e
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-16 12:31:56 +0800

    async task and loader project init

---
+++ 0216aynctaskandloader/src/main/java/com/shyam/a0216aynctaskandloader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shyam.a0216aynctaskandloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
