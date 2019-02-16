05909d0dcf9f5ed7564ae7d207120e1ee366c039
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-16 23:52:28 +0800

    send notification

--- 0216notifications/src/main/java/com/shyam/a0216notifications/MainActivity.java
+++ 0216notifications/src/main/java/com/shyam/a0216notifications/MainActivity.java
@@ -1,13 +1,62 @@
 package com.shyam.a0216notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final int NOTIFICATION_ID = 0;
+    private NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void createNotificationChannel() {
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setDescription("Notification from Code labs App.");
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified.")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        return notificationBuilder;
+    }
+
+    public void sendNotification(View view) {
+        createNotificationChannel();
+        NotificationCompat.Builder builder = getNotificationBuilder();
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
     }
 }
