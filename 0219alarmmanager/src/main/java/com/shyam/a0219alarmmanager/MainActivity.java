385606b83089b5acbac6e3a99c74d5a723ed5bab
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-20 01:16:31 +0800

    add data binding to handle toggle

--- 0219alarmmanager/src/main/java/com/shyam/a0219alarmmanager/MainActivity.java
+++ 0219alarmmanager/src/main/java/com/shyam/a0219alarmmanager/MainActivity.java
@@ -1,13 +1,30 @@
 package com.shyam.a0219alarmmanager;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.shyam.a0219alarmmanager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivity(this);
+
+    }
+
+    public void onCheckChange(CompoundButton button, Boolean isChecked) {
+        if(isChecked) {
+            Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "UnChecked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
