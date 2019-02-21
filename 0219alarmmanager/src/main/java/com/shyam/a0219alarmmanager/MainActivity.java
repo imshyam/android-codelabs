8dd2f6e80228a807b38878cb9f73e6fd184a4de0
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-02-21 10:30:44 +0800

    use broadcast, bind toggle button

--- 0219alarmmanager/src/main/java/com/shyam/a0219alarmmanager/MainActivity.java
+++ 0219alarmmanager/src/main/java/com/shyam/a0219alarmmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shyam.a0219alarmmanager;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
@@ -20,13 +22,25 @@ public class MainActivity extends AppCompatActivity {
     private  NotificationManager notificationManager;
     private final static int NOTIFICATION_ID = 0;
     private final static String NOTIFICATION_CHANNEL_ID = "primary_channel_id";
+    private AlarmManager alarmManager;
+    private PendingIntent notifyPendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setActivity(this);
-
         createNotificationChannel();
+
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
+        notifyPendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        boolean alarmUp = (PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent,
+                PendingIntent.FLAG_NO_CREATE) != null);
+
+        binding.setChecked(alarmUp);
 
     }
 
@@ -45,28 +59,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void deliverNotification(Context context) {
-        Intent contentIntent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setContentTitle("Stand Up Alert")
-                .setContentText("You should stand up and walk around now!")
-                .setSmallIcon(R.drawable.ic_walk_notification)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL);
-        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
-    }
-
     public void onCheckChange(CompoundButton button, Boolean isChecked) {
         String toastMsg;
         if(isChecked) {
-            deliverNotification(MainActivity.this);
+            long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+            long triggerTime = SystemClock.elapsedRealtime();
+            if(alarmManager != null && notifyPendingIntent != null) {
+                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        triggerTime,
+                        repeatInterval,notifyPendingIntent);
+            }
             toastMsg = "Alarm On";
         } else {
-            notificationManager.cancelAll();
+            alarmManager.cancel(notifyPendingIntent);
             toastMsg = "Alarm Off";
         }
         Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show();
